539efb838130ce23734cbe21b6ea118a1fada206
Lynn Langit <lynnlangit@live.com>
2020-09-25 13:17:08 -0500

    add node js on GCE install instructions

---
+++ 2_compute/2a_Compute Engine/install-node-on-gce.sh
@@ -0,0 +1,5 @@
+# -- to install Node JS on a GCE default instance -- #
+
+1. Create a default GCE instance and SSH to that instance
+2. Install Node JS via the SSH terminal window using `sudo apt install nodejs`
+3. Verify installation using `node -v`
